a70f8d3ca95c9d38b6bc686633fb921db0338621
Mikechan <m7807031@gmail.com>
2019-09-25 19:46:30 +0800

    first commit.

---
+++ xlsx2docx.py
@@ -0,0 +1,39 @@
+# -*-coding: utf-8-*-
+# editor: MikeChan
+# email: m7807031@gmail.com
+# license: BSD-3
+
+# OS import
+import os
+import random
+from datetime import datetime, date
+import time
+import re
+
+# Office-docx lib import
+import docx
+from docx.shared import Cm, Pt  #add unit for word
+from docx.enum.text import WD_ALIGN_PARAGRAPH # deal with alignment
+from docx.shared import RGBColor
+
+# ===== TODO ===== #
+# 1. excel to docx tableformat
+# 2. change file name
+
+# ===== Global Variables ===== #
+
+
+# ===== Global Function ===== #
+
+
+# ====== Functions ===== #
+
+
+# ====== main() ===== #
+def main():
+    pass
+
+
+
+if __name__=="__main__":
+    main()
